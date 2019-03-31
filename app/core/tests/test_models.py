70f24d537ba5cc9aa32475efb5037fe89c50472f
ripndip121 <j.pena1386@gmail.com>
2019-03-31 07:02:05 -0400

    syntax corrections

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 
-class ModleTests(TestCase):
+class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         """Test creating a new user with email"""
@@ -14,7 +14,7 @@ class ModleTests(TestCase):
         self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalized(self):
-        email = 'test@PROTONMAIL.COM'
+        email = 'test@EXAMPLE.COM'
         user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
